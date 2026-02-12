0129e30ba916c50b8cc9910783fd4c3d390910d9
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-02-12 17:09:14 +0200

    feat: Implement notes module with CRUD operations, pagination, sorting, and authentication.

--- src/Modules/Notes/Notes.controller.js
+++ src/Modules/Notes/Notes.controller.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import * as notesService from "./Notes.service.js";
 const router = Router();
 
+router.get("/paginate-sort", notesService.paginateSortNotes);
 router.post("/", notesService.addNote);
 router.patch("/all", notesService.updateAllNotesTitle);
 router.patch("/:noteId", notesService.updateSingleNote);
 router.put("/replace/:noteId", notesService.replaceNoteDocument);
+router.delete("/:noteId", notesService.deleteNote);
 
 export default router;
--- src/Modules/Notes/Notes.service.js
+++ src/Modules/Notes/Notes.service.js
@@ -93,3 +93,63 @@ export const updateAllNotesTitle = async (req, res) => {
         return res.status(500).json({ message: error.message, status: "error", stack: error.stack });
     }
 }
+export const deleteNote = async (req, res) => {
+    try {
+        const token = req.headers['authorization'];
+        if (!token) return res.status(401).json({ message: "Unauthorized", status: "error" })
+        const decodedToken = jwt.verify(token, jwtSecret);
+        const user = await UserModel.findById(decodedToken.id);
+        if (!user) return res.status(404).json({ message: "User Not Found ", status: "error" });
+        if (token !== user.token) return res.status(401).json({ message: "Unauthorized: Invalid token", status: "error" });
+
+        const { noteId } = req.params;
+        const noteOwner = await NoteModel.findById(noteId);
+        if (!noteOwner) return res.status(404).json({ message: "Note not found", status: "error" });
+        if (noteOwner.userId.toString() !== decodedToken.id) return res.status(401).json({ message: "Unauthorized: Invalid token", status: "error", });
+        const note = await NoteModel.findByIdAndDelete(noteId);
+        if (!note) return res.status(404).json({ message: "Note not found", status: "error" });
+
+        return res.status(200).json({ message: "Note Deleted successfully", status: "success", note });
+    } catch (error) {
+        return res.status(500).json({ message: error.message, status: "error", stack: error.stack });
+    }
+}
+export const paginateSortNotes = async (req, res) => {
+    try {
+        const token = req.headers['authorization'];
+        if (!token) return res.status(401).json({ message: "Unauthorized", status: "error" })
+        const decodedToken = jwt.verify(token, jwtSecret);
+        const user = await UserModel.findById(decodedToken.id);
+        if (!user) return res.status(404).json({ message: "User Not Found ", status: "error" });
+        if (token !== user.token) return res.status(401).json({ message: "Unauthorized: Invalid token", status: "error" });
+
+        let { page, limit } = req.query;
+        page = parseInt(page) || 1;
+        limit = parseInt(limit) || 10;
+        if (page < 1) page = 1;
+        if (limit < 1) limit = 10;
+
+        const query = { userId: decodedToken.id };
+        let sortOption = { createdAt: -1 };
+        const notes = await NoteModel.find(query)
+            .sort(sortOption)
+            .skip((page - 1) * limit)
+            .limit(limit);
+        const totalNotes = await NoteModel.countDocuments(query);
+        const totalPages = Math.ceil(totalNotes / limit);
+
+        return res.status(200).json({
+            message: "Notes fetched successfully",
+            status: "success",
+            notes,
+            pagination: {
+                totalNotes,
+                totalPages,
+                currentPage: page,
+                itemsPerPage: limit
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message, status: "error", stack: error.stack });
+    }
+}
