438851ab6aa3c9551a136999d255ae85bae50768
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-02-13 17:49:52 +0200

    bonus file

---
+++ bonus.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if (!strs.length) return "";
+    strs.sort();
+    let first = strs[0], last = strs[strs.length - 1], result = "";
+    for (let i = 0; i < first.length; i++) {
+        if (first[i] === last[i]) {
+            result += first[i];
+        } else {
+            break;
+        }
+    }
+    return result;
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
